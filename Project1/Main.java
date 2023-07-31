7ec3a9acb4561b92feed97bbd0f584f914b37ac4
Bashaer <110439978+bashaer310@users.noreply.github.com>
2023-07-31 18:52:09 +0300

    Update project1

--- Project1/Main.java
+++ Project1/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-
         Scanner input = new Scanner(System.in);
         char [][] tictactoe = new char[3][3];
         boolean finished=false;
