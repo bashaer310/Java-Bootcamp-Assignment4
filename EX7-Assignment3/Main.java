4c22b2c1e2cc7dc947f7ca06b102399f63d96434
Bashaer <110439978+bashaer310@users.noreply.github.com>
2023-07-31 18:53:43 +0300

    Update EX7-Assignment3

--- EX7-Assignment3/Main.java
+++ EX7-Assignment3/Main.java
@@ -2,7 +2,6 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
 public class Main {
     public static void main(String[] args) {
         //Q7.Write a menu driven Java program with following option:
